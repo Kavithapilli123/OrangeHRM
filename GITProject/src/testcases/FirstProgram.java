8a06a04b7790a9854cbe533bbc18a3d3c0666103
Kavit <Kavit@LAPTOP-4AGE3FVM>
2022-10-18 22:25:11 +0530

    First Commit:Testcase 1 added

---
+++ GITProject/src/testcases/FirstProgram.java
@@ -0,0 +1,11 @@
+package testcases;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("hello my first program");
+		
+
+	}
+
+}
---
+++ GITProject/src/testcases/SecondProgram.java
@@ -0,0 +1,10 @@
+package testcases;
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+	System.out.println("hello second program");
+
+	}
+
+}
